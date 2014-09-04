3ecbc76d021cd592bb9cf751a2acea8156d44035
JimBobBennett <jimbobbennett@mac.com>
2014-09-04 21:58:15 +0700

    added jimbobbennett-io config to separate folder

---
+++ config/jimbobbennett-io/config.js
@@ -0,0 +1,80 @@
+var fauxGhostConfig = {
+    "site_url": "http://jimbobbennett.io",
+    "google_fonts_family": "Open+Sans:400,400italic,700,700italic",
+    "google_analytics_tracking_id": "UA-51294952-1",
+    "disqus_shortname": "jimbobbennett",
+    //"show_author_details": "true",
+    "show_published_by_ghost": "true",
+    "email":
+    {
+      "address": "jim@jimbobbennett.io",
+      "name": "Send Email"
+    },
+    "rss": "true",
+    "sharing":
+    {
+      "twitter": "true",
+      "facebook": "true",
+      "googlePlus": "true"
+    },
+    "socialLinks":
+    [
+      {
+        "href": "http://twitter.com/jimbobbennett",
+        "name": "Twitter",
+        "icon": "fa-twitter-square"
+      },
+      {
+        "href": "http://github.com/jimbobbennett",
+        "name": "GitHub",
+        "icon": "fa-github-square"
+      },
+      {
+        "href": "http://www.linkedin.com/pub/jim-bennett/20/448/367",
+        "name": "LinkedIn",
+        "icon": "fa-linkedin-square"
+      },
+      {
+        "href": "http://plus.google.com/+JimBennettio",
+        "name": "Google+",
+        "icon": "fa-google-plus-square"
+      }
+    ],
+    "sidebar":
+    {
+      "title": "Main Menu",
+      "items":
+      [
+        {
+          "title": "Home",
+          "url": "http://www.jimbobbennett.io",
+          "faicon": "fa-home"
+        },
+        {
+          "title": "About",
+          "url": "http://ghost.org/jimbobbennett/",
+          "image": "../favicons/favicon.ico"
+        },
+        {
+          "title": "MooMoo.io",
+          "url": "https://www.moomoo.io",
+          "image": "Ducky.svg"
+        },
+        {
+          "title": "Technology",
+          "tag": "technology",
+          "faicon": "fa-desktop"
+        },
+        {
+          "title": "Business",
+          "tag": "business",
+          "faicon": "fa-briefcase"
+        },
+        {
+          "title": "Food",
+          "tag": "food",
+          "faicon": "fa-cutlery"
+        }
+      ]
+    }
+}
